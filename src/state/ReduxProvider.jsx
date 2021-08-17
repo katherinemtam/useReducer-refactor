9bcfe8af258413e010963311ed5457976509e9fc
Katherine Tam <katherinemtam@gmail.com>
2021-08-17 09:49:58 -0700

    return object of values

--- src/state/ReduxProvider.jsx
+++ src/state/ReduxProvider.jsx
@@ -7,7 +7,7 @@ export const ReduxProvider = ({ reducer, initialState, children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <ReduxContext.Provider value={state, dispatch}>
+    <ReduxContext.Provider value={{ state, dispatch }}>
       {children}
     </ReduxContext.Provider>
   );
