1b9d950c91f43e3530a66bf1dc69328a34e854e6
Katherine Tam <katherinemtam@gmail.com>
2021-08-17 10:38:41 -0700

    add ReduxProvider and change initial state to App.test, test passes

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
+import { ReduxProvider } from '../../state/ReduxProvider';
+import { initialState, reducer } from '../../state/reducer';
 
 describe('App component', () => {
   it('renders App and displays colors', () => {
-    render(<App />);
+    render(
+      <ReduxProvider reducer={reducer} initialState={initialState} >
+        <App />
+      </ReduxProvider>);
   
     const display = screen.getByTestId('display');
 
@@ -17,7 +22,7 @@ describe('App component', () => {
     //test undo button
     const undoButton = screen.getByTestId('undoButton');
     userEvent.click(undoButton);
-    expect(display).toHaveStyle({ backgroundColor: '#FF0000' });
+    expect(display).toHaveStyle({ backgroundColor: '#8cd3ff' });
 
     //test redo button
     const redoButton = screen.getByTestId('redoButton');
