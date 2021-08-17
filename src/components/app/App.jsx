147d87265c4189bbd1bafa2f45ffdf0c6ff9f7d2
Katherine Tam <katherinemtam@gmail.com>
2021-08-17 09:52:00 -0700

    remove useState code, invoke dispatch functions

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,52 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { setUndo, setRecord, setRedo } from '../../state/actions';
-import { useDispatch, useSelector } from '../../state/ReduxProvider';
-import { selectAfter, selectBefore, selectCurrent } from '../../state/selectors';
-
-const useRecord = (init) => {
-  const before = useSelector(selectBefore);
-  const current = useSelector(selectCurrent);
-  const after = useSelector(selectAfter);
-  const dispatch = useDispatch();
-
-  useEffect(async () => {
-    dispatch(getBefore(before));
-    dispatch(getCurrent(current));
-    dispatch(getAfter(after));
-  }, []);
-
-  // const [before, setBefore] = useState([]);
-  // const [current, setCurrent] = useState(init);
-  // const [after, setAfter] = useState([]);
-
-  const undo = () => {
-  //   setAfter((after) => [current, ...after]);
-  //   setCurrent(before[before.length - 1]);
-  //   setBefore((before) => before.slice(0, -1));
-    dispatch(setUndo(before));
-  };
-
-  const redo = () => {
-  //   setBefore((before) => [...before, current]);
-  //   setCurrent(after[0]);
-  //   setAfter((after) => after.slice(1));
-    dispatch(setRedo(after))
-  };
-
-  const record = (val) => {
-    // setBefore((before) => [...before, current]);
-    // setCurrent(val);
-    dispatch(setRecord(val));
-  }
-  };
-
-  return {
-    undo,
-    record,
-    redo,
-    current,
-  };
-};
+import React from 'react';
+import { setRecord, setRedo, setUndo } from '../../state/actions.js';
+import { useDispatch, useSelector } from '../../state/ReduxProvider.jsx';
+import { selectCurrent } from '../../state/selectors.js';
 
 function App() {
   // const before = useSelector(selectBefore);
@@ -54,16 +9,14 @@ function App() {
   // const after = useSelector(selectAfter);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(setRecord('#fac6e5'));
-  });
+  console.log('current', current);
 
   const undo = () => {
-    dispatch(setUndo);
+    dispatch(setUndo());
   };
 
   const redo = () => {
-    dispatch(setRedo);
+    dispatch(setRedo());
   };
 
   const record = (current) => {
